7358ac82c9a94de604144329dd2ffdc048f2e9fc
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-12 11:21:47 +0530

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,16 @@ import { db } from "../../drizzle";
 import * as schema from "../../drizzle/schema";
 
 export const auth = betterAuth({
+    socialProviders: {
+        github: {
+            clientId: process.env.GITHUB_CLIENT_ID as string,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+        },
+        google: {
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        },
+    },
     emailAndPassword: {
         enabled: true,
     },
