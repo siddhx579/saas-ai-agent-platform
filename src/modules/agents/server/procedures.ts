cc509747145ed17d8d6cc9b214d005062b24ee8f
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-20 10:05:51 +0530

    Update procedures.ts

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,12 +1,33 @@
-import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { db } from "../../../../drizzle";
 import { agents } from "../../../../drizzle/schema";
+import { agentsInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
+    getOne: baseProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
+        const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id));
+        await new Promise((resolve) => setTimeout(resolve, 5000));
+
+        return existingAgent;
+    }),
     getMany: baseProcedure.query(async () => {
         const data = await db.select().from(agents);
         await new Promise((resolve) => setTimeout(resolve, 5000));
 
         return data;
     }),
+    create: protectedProcedure
+        .input(agentsInsertSchema)
+        .mutation(async ({ input, ctx }) => {
+            const [createdAgent] = await db
+                .insert(agents)
+                .values({
+                    ...input,
+                    userId: ctx.auth.user.id,
+                })
+                .returning();
+            return createdAgent;
+        })
 });
