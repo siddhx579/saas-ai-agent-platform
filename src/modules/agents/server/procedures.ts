06848b34b910276d867ffc04eebf4e712f5783f4
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-20 12:16:40 +0530

    Update procedures.ts

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,21 +3,32 @@ import { db } from "../../../../drizzle";
 import { agents } from "../../../../drizzle/schema";
 import { agentsInsertSchema } from "../schemas";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
-    getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-        const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id));
-        await new Promise((resolve) => setTimeout(resolve, 5000));
+    getOne: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .query(async ({ input }) => {
+            const [existingAgent] = await db
+                .select({
+                    meetingCount: sql<number>`5`,
+                    ...getTableColumns(agents),
+                })
+                .from(agents)
+                .where(eq(agents.id, input.id));
+            await new Promise((resolve) => setTimeout(resolve, 5000));
 
-        return existingAgent;
-    }),
-    getMany: protectedProcedure.query(async () => {
-        const data = await db.select().from(agents);
-        await new Promise((resolve) => setTimeout(resolve, 5000));
+            return existingAgent;
+        }),
+    getMany: protectedProcedure
+        .query(async () => {
+            const data = await db
+                .select()
+                .from(agents);
+            await new Promise((resolve) => setTimeout(resolve, 5000));
 
-        return data;
-    }),
+            return data;
+        }),
     create: protectedProcedure
         .input(agentsInsertSchema)
         .mutation(async ({ input, ctx }) => {
