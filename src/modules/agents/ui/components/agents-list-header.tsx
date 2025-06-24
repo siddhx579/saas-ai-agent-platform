c85bb2ea07f99b740f7f4156e74bff113aa7c978
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-24 12:32:52 +0530

    Update agents-list-header.tsx

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -4,8 +4,11 @@ import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
 import { NewAgentDialog } from "./new-agent-dialog";
 import { useState } from "react";
+import { useAgentsFilters } from "../../hooks/use-agents-filters";
+import { AgentsSearchFilter } from "./agents-search-filter";
 
 export const AgentsListHeader = () => {
+    const [filters, setFilters] = useAgentsFilters();
     const [isDialogOpen, setIsDialogOpen] = useState(false);
 
     return (
@@ -18,6 +21,9 @@ export const AgentsListHeader = () => {
                         <PlusIcon />
                         New Agent
                     </Button>
+                </div>
+                <div className="flex items-center gap-x-2 p-1">
+                    <AgentsSearchFilter />
                 </div>
             </div>
         </>
