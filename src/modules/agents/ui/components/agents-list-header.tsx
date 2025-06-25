14712020cb53ca9b6a00f3c6687daa1f7848231e
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-25 12:17:27 +0530

    Update agents-list-header.tsx

--- src/modules/agents/ui/components/agents-list-header.tsx
+++ src/modules/agents/ui/components/agents-list-header.tsx
@@ -1,15 +1,23 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { PlusIcon } from "lucide-react";
+import { PlusIcon, XCircleIcon } from "lucide-react";
 import { NewAgentDialog } from "./new-agent-dialog";
 import { useState } from "react";
 import { useAgentsFilters } from "../../hooks/use-agents-filters";
 import { AgentsSearchFilter } from "./agents-search-filter";
+import { DEFAULT_PAGE } from "@/constants";
 
 export const AgentsListHeader = () => {
     const [filters, setFilters] = useAgentsFilters();
     const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const isAnyFilterModified = !!filters.search;
+    const onClearFilters = () => {
+        setFilters({
+            search: "",
+            page: DEFAULT_PAGE,
+        });
+    }
 
     return (
         <>
@@ -24,6 +32,12 @@ export const AgentsListHeader = () => {
                 </div>
                 <div className="flex items-center gap-x-2 p-1">
                     <AgentsSearchFilter />
+                    {isAnyFilterModified && (
+                        <Button variant="outline" size="sm" onClick={onClearFilters}>
+                            <XCircleIcon />
+                            Clear
+                        </Button>
+                    )}
                 </div>
             </div>
         </>
