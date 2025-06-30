b3f0a0293044722a8d49702fe98612ef1cadccc0
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-06-30 12:13:15 +0530

    Update agent-id-view.tsx

--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -10,6 +10,9 @@ import { Badge } from "@/components/ui/badge";
 import { VideoIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { useConfirm } from "@/hooks/use-confirm";
+import { useState } from "react";
+import { UpdateAgentDialog } from "../components/update-agent-dialog";
 
 interface Props {
     agentId: string;
@@ -18,6 +21,7 @@ interface Props {
 export const AgentIdView = ({ agentId }: Props) => {
     const router = useRouter();
     const queryClient = useQueryClient();
+    const [updateAgentDialogOpen, setUpdateAgentDialogOpen] = useState(false);
     const trpc = useTRPC();
     const { data } = useSuspenseQuery(trpc.agents.getOne.queryOptions({ id: agentId }));
     const removeAgent = useMutation(
@@ -32,38 +36,59 @@ export const AgentIdView = ({ agentId }: Props) => {
         }),
     );
 
+    const [RemoveConfirmation, confirmRemove] = useConfirm(
+        "Are you sure?",
+        `The following action will remove ${data.meetingCount} associated meetings`,
+    );
+
+    const handleRemoveAgent = async () => {
+        const ok = await confirmRemove();
+
+        if (!ok) return;
+
+        await removeAgent.mutateAsync({ id: agentId });
+    };
+
     return (
-        <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
-            <AgentIdViewHeader
-                agentId={agentId}
-                agentName={data.name}
-                onEdit={() => { }}
-                onRemove={() => removeAgent.mutate({ id: agentId })}
+        <>
+            <RemoveConfirmation />
+            <UpdateAgentDialog
+                open={updateAgentDialogOpen}
+                onOpenChange={setUpdateAgentDialogOpen}
+                initialValues={data}
             />
-            <div className="bg-white rounded-lg border">
-                <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
-                    <div className="flex items-center gap-x-3">
-                        <GeneratedAvatar
-                            variant="botttsNeutral"
-                            seed={data.name}
-                            className="size-10"
-                        />
-                        <h2 className="text-2xl font-medium">{data.name}</h2>
-                    </div>
-                    <Badge
-                        variant="outline"
-                        className="flex items-center gap-x-2 [&>svg]:size-4"
-                    >
-                        <VideoIcon className="text-blue-700" />
-                        {data.meetingCount} {data.meetingCount === 1 ? "meeting" : "meetings"}
-                    </Badge>
-                    <div className="flex flex-col gap-y-4">
-                        <p className="text-lg font-medium">Instructions</p>
-                        <p className="text-neutral-800">{data.instructions}</p>
+            <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+                <AgentIdViewHeader
+                    agentId={agentId}
+                    agentName={data.name}
+                    onEdit={() => setUpdateAgentDialogOpen(true)}
+                    onRemove={handleRemoveAgent}
+                />
+                <div className="bg-white rounded-lg border">
+                    <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+                        <div className="flex items-center gap-x-3">
+                            <GeneratedAvatar
+                                variant="botttsNeutral"
+                                seed={data.name}
+                                className="size-10"
+                            />
+                            <h2 className="text-2xl font-medium">{data.name}</h2>
+                        </div>
+                        <Badge
+                            variant="outline"
+                            className="flex items-center gap-x-2 [&>svg]:size-4"
+                        >
+                            <VideoIcon className="text-blue-700" />
+                            {data.meetingCount} {data.meetingCount === 1 ? "meeting" : "meetings"}
+                        </Badge>
+                        <div className="flex flex-col gap-y-4">
+                            <p className="text-lg font-medium">Instructions</p>
+                            <p className="text-neutral-800">{data.instructions}</p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
