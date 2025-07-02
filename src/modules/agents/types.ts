d8f6e38285bb1e8e734fe79357280d8b74760a00
Siddhant Kapoor <109038046+siddhx579@users.noreply.github.com>
2025-07-02 10:43:59 +0530

    Update types.ts

--- src/modules/agents/types.ts
+++ src/modules/agents/types.ts
@@ -2,3 +2,4 @@ import { AppRouter } from "@/trpc/routers/_app";
 import { inferRouterOutputs } from "@trpc/server";
 
 export type AgentGetOne = inferRouterOutputs<AppRouter>["agents"]["getOne"];
+export type AgentsGetMany = inferRouterOutputs<AppRouter>["agents"]["getMany"]["items"];
